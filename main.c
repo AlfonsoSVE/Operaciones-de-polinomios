6f15aa7477a7b0dd5936d21f347c7072d53eb237
Sánchez del Valle Escanero Alfonso <54370015+AlfonsoSVE@users.noreply.github.com>
2019-11-24 20:47:43 -0600

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,38 @@
 #include 'funciones.h'
 
 int main(){
-  polino *hdnum,*hdden,*apuntanum;
-	int n1,n2,i,j,expo;
+  polino *head1,*head2,*res;
+	int n1,n2,i,j,expo, op;
 	float coef;
-	int b_salir;
-	char respuesta[1];
 
-	b_salir=0;
-	while (b_salir!=1) {
+
+	n1=void scan_polinomio(polino a);
+	n2=void scan_polinomio(polino a);
+	void orden_poliniomio(polino *lista);
+	do{
+		menu();
+		scanf("%d", &op);
+		switch(op){
+			case 1:
+				res= suma(a, b);
+				break;
+			case 2:
+				res=restar(a, b, c);
+				break;
+			case 3:
+				res=multiplicar(a, b, c);
+				break;
+			case 4:
+				res= dividir(a, b, c);
+				break;
+			case 5:
+				res=division_sintetica(a, b);
+				break;
+			case 6:
+				printf("Goodbye\n");
+				break;
+			default:
+				printf("Opcion no valido\n");
+		}
+		print_polinomio(a)=res;
+	}while(op<7);
