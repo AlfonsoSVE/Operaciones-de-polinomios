98256db4ed4ec0ca9ff8e18cebe13d7b97b910ee
Sánchez del Valle Escanero Alfonso <54370015+AlfonsoSVE@users.noreply.github.com>
2019-11-24 20:26:33 -0600

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,1 +1,23 @@
+#ifdef 	FUNCIONES_H
+#define FUNCIONES_H
 
+#include <stdio.h>
+#include "polinomios.h"
+struct polinomios{
+	float coeficiente;
+	int exponente;
+	struct polinomios *siguiente;
+};
+typedef struct polinomios polino;
+
+void scan_polinomio(polino a, polino b);
+void orden_poliniomio(polino *lista);
+void dividir(polino *numerador,polino *denominador);
+void multiplicar(polino *multiplicando ,polino *multiplicador,polino *producto);
+void restar(polino *minuendo, polino *sustraendo ,polino *diferencia);
+void compara(polino *polinomio1,polino *polinomio2 );
+void division_sintetica(polino *monomio, polino *polinomio);
+void menu();
+void print_polinomio(polino a, polino b);
+
+#endif
