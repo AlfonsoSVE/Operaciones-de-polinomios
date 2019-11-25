1755fe515564d24c5856cd69c3009d3f9351ea35
Sánchez del Valle Escanero Alfonso <54370015+AlfonsoSVE@users.noreply.github.com>
2019-11-24 20:28:40 -0600

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -10,7 +10,7 @@ struct polinomios{
 };
 typedef struct polinomios polino;
 
-void scan_polinomio(polino a, polino b);
+void scan_polinomio(polino a);
 void orden_poliniomio(polino *lista);
 void dividir(polino *numerador,polino *denominador);
 void multiplicar(polino *multiplicando ,polino *multiplicador,polino *producto);
