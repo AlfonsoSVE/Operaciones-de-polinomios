028c57014eae8a9c9a44cfe3ed732ac511bb816a
Sánchez del Valle Escanero Alfonso <54370015+AlfonsoSVE@users.noreply.github.com>
2019-11-24 19:55:18 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include 'funciones.h'
 
+int main(){
+  polino *hdnum,*hdden,*apuntanum;
+	int n1,n2,i,j,expo;
+	float coef;
+	int b_salir;
+	char respuesta[1];
+
+	b_salir=0;
+	while (b_salir!=1) {
