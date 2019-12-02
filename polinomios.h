962a0bc7aac457c9d14bad1319f25898d4537667
Sánchez del Valle Escanero Alfonso <54370015+AlfonsoSVE@users.noreply.github.com>
2019-12-02 12:33:38 -0600

    Update polinomios.h

--- polinomios.h
+++ polinomios.h
@@ -1,1 +1,13 @@
 
+typedef struct polinomios polino;
+
+struct polinomios{
+	float coeficiente;
+	int exponente;
+	polino *siguiente;
+};
+
+struct monomio{
+	float coeficiente;
+	int exponente;
+};
