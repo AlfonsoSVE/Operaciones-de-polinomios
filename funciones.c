51951b904a374f0fec2fb37bd62de184ab1c9320
Sánchez del Valle Escanero Alfonso <54370015+AlfonsoSVE@users.noreply.github.com>
2019-12-04 12:17:34 -0600

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -46,40 +46,87 @@ void suma(polino *a, polino *b){
 
 void dividir(polino *a,polino *b, polino *cociente){
 
-	polino *header_b, *header_d, *header_c  *header_a, *Headerb_prima; 	
-	polino *b_prima,*d,*apuntador;
+	polino *header_a, *header_d, *header_c , *header_b, *Headera_prima; 	
+	polino *a_prima,*d,*apuntador;
 	struct nomio factor;
 	int resultado;
-	int i,na,nb,nd;
-
-		while (numerador->siguiente!=NULL) {
-			while (denominador->siguiente!=NULL) {
-				resultado=compara(a,b);
-				if (c==1) {
-					if (numerador->exponente>=denominador->exponente) {
-						cociente=(polino*)malloc(sizeof(polino));
-						cociente->coeficiente=numerador->coeficiente/denominador->coeficiente;
-						cociente->exponente=numerador->exponente-denominador->exponente;
-						sustraendo=multiplicar(cociente,denominador,sustraendo);
-						residuo=restar(numerador,sustraendo,residuo);
-						resultado=compara(residuo,denominador,resultado);
-						if (resultado==1) {
-							numerador=residuo;
+	int i,na,nb,nd, b_fin;
+	header_a=a;
+	header_b=b;
+	na=cuenta(a);
+	nb=cuenta(b);
+	header_c=(polino*)malloc(sizeof(polino));
+	cociente=header_c;
+	for (i = 0; i < na; i++) {
+		cociente->siguiente=(polino*)malloc(sizeof(polino));
+		cociente=cociente->siguiente;
+	}
+	Headera_prima=(polino*)malloc(sizeof(polino));
+	a_prima=Headera_prima;
+	for (i = 0; i < nd; i++) {
+		a_prima->siguiente=(polino*)malloc(sizeof(polino));
+		a_prima=a_prima->siguiente;
+	}
+	header_d=(polino*)malloc(sizeof(polino));
+	d=header_d;
+	if (na>nb) {
+		nd=na;
+	}else
+		nd=nb;
+	for (i = 0; i < nd; i++) {
+		d->siguiente=(polino*)malloc(sizeof(polino));
+		d=d->siguiente;
+	}
+  	cociente=header_c;
+	resultado=1;
+	While(resultado==1){
+		resultado=0;
+		b_fin=0;
+		while ((a->siguiente!=NULL) && (b_fin==0)) {
+			if (a->coeficiente!=0) {
+				while ((b->siguiente!=0) && (b_fin==0)) {
+					if (b->coeficiente!=0) {
+						if (a->exponente>b->exponente) {
+							resultado=1;
+							b_fin=1;
 						}
-						else{
-							printf("Resultado de la division\n");
-							c=cociente;
-						
+						else {
+							if (a->exponente==b->exponente) {
+								if (a->coeficiente>=b->coeficiente)
+									resultado=1;
+								b_fin=1;
+							}
+							else
+								b_fin=1;
 						}
-					}	
+					}
+					b=b->siguiente;
 				}
 			}
+			else
+				a=a->siguiente;
 		}
+		while ((a->coeficiente==0) && (a->siguiente!=NULL)) {
+			a=a->siguiente;
+		}
+		cociente->coeficiente=a->coeficiente/b->coeficiente;
+		cociente->exponente=a->exponente-b->exponente;
+		factor.coeficiente=cociente->coeficiente;
+		factor.exponente=cociente->exponente;
+		cociente=cociente->siguiente;
+		a_prima=Headera_prima;
+		while (denominador->siguiente!=NULL) {
+			a_prima->coeficiente=factor.coeficiente*b->coeficiente;
+			a_prima->exponente=factor.exponente+b->exponente;
+			a_prima=a_prima->siguiente;
+			b=b->siguiente;
+		}
+		d=header_d;
+		restar(a,a_prima, d);
+		a=d;
 	}
-  return c;
+	return header_d, header_c; 
 }
-
-
 
 void multiplicar(polino *a ,polino *b,polino *c){
   
