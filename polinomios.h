37fd16a4df07c69a42e1bc9c33e27fa4c6f6b80b
Missael-26 <54322439+Missael-26@users.noreply.github.com>
2019-12-07 22:01:14 -0600

    Update polinomios.h

--- polinomios.h
+++ polinomios.h
@@ -16,3 +16,28 @@ struct Polinomio
    float *p;
    int grado;
 };
+void lee_polinomio(struct Polinomio* pol)
+{
+   int i;
+
+   puts("Grado:"); fflush(stdout);
+   scanf("%d",&pol->exponente);
+
+   pol->p= malloc( (pol->exponente+1) * sizeof(float));
+
+   for( i = 0; i <= pol->exponente; i++ )
+   {
+       printf("Introduzca el valor del coeficiente %d:\n",i);
+       scanf("%f",&pol->p[i]);
+   }     
+}
+void muestra_polinomios(struct polinomios* pol )
+{
+   int i;
+   printf("El grado del polinomio es %d\n", pol->exponente );
+printf("Los coeficientes correspondientes a los grados de 0 hasta %d, respectivamente son:\n", pol->exponente);
+   for( i = 0; i < pol->exponente; i++ )
+      printf("%4.2f, x^%i ", pol->p[i],i );
+   printf("%4.2f", pol->p[i] );
+   putchar('\n');
+}
