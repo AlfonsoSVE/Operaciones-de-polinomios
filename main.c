cb99c00a9f539fabe98cbc53da202705f343446e
Sánchez del Valle Escanero Alfonso <54370015+AlfonsoSVE@users.noreply.github.com>
2019-12-08 19:13:56 -0600

    Update main.c

--- main.c
+++ main.c
@@ -7,69 +7,68 @@ int main(){
 	float coef;
 
 	do{
-		crear_archivo(a);
+		//crear_archivo(a);
 		menu();
 		scanf("%d", &op);
 		switch(op){
 			case 1:
 				a=scan_polinomio(a);
 				a=orden_poliniomio(a);
-				guardar_archivo(a);
+				//guardar_archivo(a);
 				break;
 			case 2: 
 				a=scan_polinomio(a);
 				a=orden_poliniomio(a);
-				guardar_archivo(a);
+				//guardar_archivo(a);
 				b=scan_polinomio(a);
 				b=orden_poliniomio(b);
-				guardar_archivo(b);
+				//guardar_archivo(b);
 				res= suma(a, b, c);
-				guardar_archivo(res);
+				//guardar_archivo(res);
 				break;
 			case 3:
 				a=scan_polinomio(a);
 				a=orden_poliniomio(a);
-				guardar_archivo(a);
+				//guardar_archivo(a);
 				b=scan_polinomio(a);
 				b=orden_poliniomio(b);
-				guardar_archivo(b);
+				//guardar_archivo(b);
 				res=restar(a, b, c);
-				guardar_archivo(res);
+				//guardar_archivo(res);
 				break;
 			case 4:
 				a=scan_polinomio(a);
 				a=orden_poliniomio(a);
-				guardar_archivo(a);
+				//guardar_archivo(a);
 				b=scan_polinomio(a);
 				b=orden_poliniomio(b);
-				guardar_archivo(b);
+				//guardar_archivo(b);
 				res=multiplicar(a, b, c);
-				guardar_archivo(res);
+				//guardar_archivo(res);
 				break;
 			case 5:
 				a=scan_polinomio(a);
 				a=orden_poliniomio(a);
-				guardar_archivo(a);
+				//guardar_archivo(a);
 				b=scan_monomio(a);
-				guardar_archivo(b);
+				//guardar_archivo(b);
 				res=division_sintetica(a, b, c);
-				guardar_archivo(res);
+				//guardar_archivo(res);
 				break;
 			case 6:
 				a=scan_polinomio(a);
-				na=cuenta(a);
 				a=orden_poliniomio(a);
-				guardar_archivo(a);
+				//guardar_archivo(a);
 				b=scan_polinomio(a);
 				b=orden_poliniomio(b);
-				guardar_archivo(b);
+				//guardar_archivo(b);
 				res= dividir(a, b, c);
-				guardar_archivo(res);
+				//guardar_archivo(res);
 				break;
 			case 7:
 				a=scan_polinomio(a);
 				orden_poliniomio(a);
-				guardar_archivo(a);
+				//guardar_archivo(a);
 				print_polinomio(a);
 			case 8:
 				a=scan_polinomio(a);
