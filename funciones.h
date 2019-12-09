069773bffe0380187c973bee8fa52231e8d9869e
Sánchez del Valle Escanero Alfonso <54370015+AlfonsoSVE@users.noreply.github.com>
2019-12-08 22:01:30 -0600

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -9,15 +9,16 @@ void orden_poliniomio(polino *lista);
 void suma(polino *a, polino *b);
 void dividir(polino *a,polino *b, polino *c);
 void scan_monomio(monomio a);
-void division_sintetica(polino *a, monomio b, polino *c);
+void division_sintetica(polino *a, int b, polino *c);
 void multiplicar(polino *a ,polino *b,polino *c);
 void restar(polino *a, polino *b ,polino *c);
 void menu();
 void print_polinomio(polino *a);
 int cuenta(polino *a);
-FILE crear_archivo(polino *a);
-FILE guardar_archivo(polino *a);
-FILE abrir_archivo(polino *polinomios);
+void borrar_polino(polino *a);
+//FILE crear_archivo(polino *a);
+//FILE guardar_archivo(polino *a);
+//FILE abrir_archivo(polino *polinomios);
 
 
 #endif
